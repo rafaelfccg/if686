64b7c7b214e29cc981e295420943a9e447d01353
rafaelfccg <rafaelfccg>
2015-03-23 17:14:24 -0300

    add heapsort

---
+++ heapsort.hs
@@ -0,0 +1,44 @@
+
+add:: Int->[Int]->[Int]
+add n hp| hp==[] = [n]
+		| otherwise = (bubbleUp (hp++[n]) ((length hp)) 0)
+
+
+bubbleUp :: [Int]->Int->Int->[Int]
+bubbleUp [] l p  = []
+bubbleUp (x:xs) l p | xs==[]|| p==l = [x] 
+					|((2*p)+1) == l && (x < (xs!!((l-p)-1))) = ([(xs!!((l-p)-1))]++(take (l-p-1) xs))++[x]
+		 		 	|  ((2*p)+2) == l && x < (xs!!((l-p)-1)) = ([xs!!((l-p)-1)]++(take (l-p-1) xs))++[x]
+		 		 	| ((2*p)+1) == l || ((2*p)+2) == l = x:xs
+		 		  	| otherwise =  x:(bubbleUp xs l (p+1))
+		 		  	
+
+remove::[Int]-> [Int]
+remove [] = []
+remove [x] = []
+remove (x:y:z:xs) | z>y = (add y (z:xs))
+			 	  | otherwise = (add z (y:xs))
+remove [x,y] = [y]
+
+
+sort ::[Int]->[Int]
+sort hp | hp == [] = []
+		| otherwise = (sort (remove hp))++[(head hp)]
+		
+
+makeheap:: [Int]->[Int]->[Int]
+makeheap [] hp  = hp
+makeheap (x:xs) hp = makeheap xs (add x hp)
+
+heapsort:: [Int]->[Int]
+heapsort l1 = sort (makeheap l1 [])
+		 		  	
+		 		  
+		 	
+
+
+
+
+
+
+ 
