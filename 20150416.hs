e66bf994ec01e201c7e312beefb0e3a1fac28be4
rafaelfccg <rafaelfccg>
2015-04-15 20:06:28 -0300

    Trabalho 8

---
+++ 20150416.hs
@@ -0,0 +1,23 @@
+--TRABALHO
+listPartitioner:: (Num a, Ord a)=>[a]->([a]->[[a]])
+listPartitioner l = partAux l
+
+partAux::(Num a,Ord a) => [a]->[a]->[[a]]
+partAux _ [] = []
+partAux [] k = [k]
+partAux (f:fs) (s:ss) | s<=f = (s:(partAux2 ss f)):(partAux fs (cut ss f)) 
+					  | otherwise =  (partAux fs (s:ss))
+
+partAux2::(Num a,Ord a) => [a]->a->[a]
+partAux2 [] _ = []
+partAux2 (f:fs) v  | f<=v = f:(partAux2 fs v)
+					    | otherwise =  []
+
+cut:: (Num a, Ord a)=> [a]->a->[a]
+cut [] _ = []
+cut (x:xs) v | x>=v = x:(cut xs v)
+             | otherwise =(cut xs v)
+-----
+
+
+
