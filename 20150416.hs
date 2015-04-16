518ad3065451bbe56b457d696d33451f48005af0
Rafael Gouveia <rfccg@cin.ufpe.br>
2015-04-16 10:03:41 -0300

    Trabalho 8 e Exercicios

--- 20150416.hs
+++ 20150416.hs
@@ -1,5 +1,6 @@
 --TRABALHO
 import Data.List (sort)
+import Data.List
 
 listPartitioner:: (Num a, Ord a)=>[a]->([a]->[[a]])
 listPartitioner l = partAux (sort l) 
@@ -20,7 +21,35 @@ cut:: (Num a, Ord a)=> [a]->a->[a]
 cut [] _ = []
 cut (x:xs) v | x>=v = x:(cut xs v)
              | otherwise =(cut xs v)
------
+----------Exercicios-
 
 
+k::(t -> u->v) -> (u->t->v)
+k f = \b a-> f a b
 
+fpairlist::([(t,t)]->[t])
+fpairlist = \l->[f |(f,s)<-l] 
+
+filterlist::([[t]]->Int->[[t]])
+filterlist = \ls v ->[m |m<-ls , (length ls )>v ]
+
+fuzelists::(Eq t)=>[[t]]->[t]
+fuzelists = \ls -> nub (foldr unioni [] ls)
+
+unioni::(Eq t)=>[t]->[t]->[t]
+unioni [] k = k
+unioni k [] = k
+unioni k (l1:l) | (l1 `elem` k) = (unioni k l)
+				| otherwise = (unioni k l)++[l1]
+-------
+
+mapfoldr:: (t->u->u)->[u]->[[t]->u]
+mapfoldr f [] = []
+mapfoldr f (x:xs) = (\l -> (foldr f x l) ):(mapfoldr f xs)
+
+---------------
+sumL :: Int -> ([Int]->[Int])
+sumL val = map (+val) 
+
+maxL :: ([Int]->Int)
+maxL =  \x -> foldr (max)  (minimum x) x
